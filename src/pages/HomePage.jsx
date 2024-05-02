70eccbdc25789c3dae67159ef63be94d001fe78d
Iryna Burlaka <i.am.blinova@gmail.com>
2024-05-02 12:31:14 +0200

    create navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import Teachers from "./TeachersPage";
+import { ReactComponent as Logo } from "../asserts/ukraine.svg";
 
 const HomePage = () => {
   return (
     <>
-      <nav></nav>
+      <nav>
+        <div>{Logo}</div>
+        <div>LearnLingo</div>
+      </nav>
       <h1>HELLO</h1>
       <ul>
         <li>Spain</li>
