07509068a8c9a1f3d52a880ea477bca08d6d6cd3
Iryna Burlaka <i.am.blinova@gmail.com>
2024-05-06 12:04:20 +0200

    nav styled completed

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ReactComponent as Logo } from "../../asserts/ukraine.svg";
 import { ReactComponent as YellowArrow } from "../../asserts/log-in-01.svg";
 // import { ReactComponent as GreenArrow } from "../../asserts/log-in-02.svg";
 // import { ReactComponent as BlouArrow } from "../../asserts/log-in-03.svg";
 // import { ReactComponent as PinkArrow } from "../../asserts/log-in-04.svg";
 
-import { ContextSection, NavigationCss, LogoCss, LinksCss } from "./Navigation.styled";
+import { ContextSection, NavigationCss, LogoCss, LinksBoxCss, NavLinkCss, WraperOfSvg, ButtonCss } from "./Navigation.styled";
 
 const Navigation = () => {
   return (
@@ -17,15 +17,17 @@ const Navigation = () => {
             <Logo />
             <div>LearnLingo</div>
           </LogoCss>
-          <LinksCss>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/teachers">Teachers</NavLink>
-          </LinksCss>
-          <div>
-            <YellowArrow />
+          <LinksBoxCss>
+            <NavLinkCss to="/">Home</NavLinkCss>
+            <NavLinkCss to="/teachers">Teachers</NavLinkCss>
+          </LinksBoxCss>
+          <LogoCss>
+            <WraperOfSvg>
+              <YellowArrow />
+            </WraperOfSvg>
             <div>Log in</div>
-            <button type="button">Registration</button>
-          </div>
+            <ButtonCss type="button">Registration</ButtonCss>
+          </LogoCss>
         </NavigationCss>
         <Outlet />
       </ContextSection>
--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 export const ContextSection = styled.section`
   padding-top: 30px;
@@ -20,10 +21,29 @@ export const LogoCss = styled.div`
   margin-right: 390px;
 `;
 
-export const LinksCss = styled.div`
-  margin-right: 270px;
+export const LinksBoxCss = styled.div`
+  margin-right: 268px;
+`;
+
+export const NavLinkCss = styled(NavLink)`
+  text-decoration: none;
+  color: black;
 
   &:first-child {
-    margin-left: 28px;
+    margin-right: 28px;
   }
+`;
+
+export const WraperOfSvg = styled.div`
+  border: dashed rgb(232, 230, 230) 1px;
+  width: 20px;
+  height: 20px;
+`;
+export const ButtonCss = styled.button`
+  display: block;
+  padding: 14px 39px;
+  border-radius: 12px;
+  border: solid rgb(18, 20, 23) 1px;
+  color: rgb(255, 255, 255);
+  background: rgb(18, 20, 23);
 `;
