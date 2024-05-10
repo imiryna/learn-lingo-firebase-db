593f8346cf1aacf3e71c85ad369f1bf15878ade2
Iryna Burlaka <i.am.blinova@gmail.com>
2024-05-10 09:50:48 +0200

    styled grid

--- src/components/HomeContetn/HomeContetn.jsx
+++ src/components/HomeContetn/HomeContetn.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Block from "../../asserts/block.jpeg";
-
+import { MainContainer, ImgCss } from "./HomeContetn.styled";
 export const HomeContetn = () => {
   return (
     <>
-      <section>
+      <MainContainer>
         <div>
-          <div>Unlock your potential with the best language tutors</div>
-          <p>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
-          <img src={Block} alt="" />
-          <button>Get started</button>
+          <div>
+            <div>Unlock your potential with the best language tutors</div>
+            <p>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
+            <button>Get started</button>
+          </div>
+
+          <ImgCss src={Block} alt="" />
         </div>
-        <div></div>
-      </section>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </MainContainer>
     </>
   );
 };
--- src/components/HomeContetn/HomeContetn.styled.js
+++ src/components/HomeContetn/HomeContetn.styled.js
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
-export const MainContainer = styled.div`
+export const MainContainer = styled.section`
   padding-top: 88px;
   padding-left: 64px;
   padding-right: 64px;
 
   display: grid;
+  gap: 24px;
+  grid-template-columns: 720px 568px;
+`;
+
+export const ImgCss = styled.img`
+  display: block;
 `;
