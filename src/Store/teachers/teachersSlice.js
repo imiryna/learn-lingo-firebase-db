c4ecd608dd6985589c1c107a58471b64c2e4f2df
Iryna Burlaka <i.am.blinova@gmail.com>
2024-06-01 14:53:08 +0200

    more fix

--- src/Store/teachers/teachersSlice.js
+++ src/Store/teachers/teachersSlice.js
@@ -11,12 +11,18 @@ const allTeachersSlice = createSlice({
   name: "teachers",
   initialState: INITIAL_STATE,
   extraReducers: (builder) => {
-    builder.addCase(getAllTeachersThunk.fulfilled, (state, action) => {
-      state.teachers = action.payload;
-      state.isLoading = false;
-      state.error = null;
-    });
-    .addCase(getAllTeachersThunk.rejected, (state, { payload }) => {
+    builder
+      .addCase(getAllTeachersThunk.fulfilled, (state, action) => {
+        state.teachers = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getAllTeachersThunk.rejected, (state, { payload }) => {
         handleRejected(state, payload);
-    }),
-}});
+      })
+      .addCase(getAllTeachersThunk.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      });
+  },
+});
